9e6c1dbea378f39dce08d91b503de40343fe0c46
Nilesh-Aditya <62688118+Nilesh-Aditya@users.noreply.github.com>
2020-09-15 03:45:21 +0530

    Update Swarm.h

--- Swarm.h
+++ Swarm.h
@@ -30,13 +30,6 @@ namespace first
         delete[] m_pParticles;
     }
 
-    // void Swarm::update()
-    // {
-    //     for (int i = 0; i < Swarm::NPARTICLES; i++)
-    //     {
-    //         m_pParticles[i].update();
-    //     }
-    // }
 
     void Swarm::update(int elapsed)
     {
